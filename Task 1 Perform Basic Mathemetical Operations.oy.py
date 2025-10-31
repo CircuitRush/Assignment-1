5a0e29e003e16afb744ec9311525155bcf36f1b5
CircuitRush <rut29@myyahoo.com>
2025-10-31 09:49:00 +0530

    This is my first assignment

---
+++ Task 1 Perform Basic Mathemetical Operations.oy.py
@@ -0,0 +1,10 @@
+x=int(input("Enter the first Number :"))
+y=int(input("Enter the second Number :"))
+Addition=x+y
+Subscration=x-y
+Multiplication=x*y
+Division=x/y
+print("Sum is " ,Addition)
+print("Subscration is ",Subscration)
+print("Multiplication is ",Multiplication)
+print("Division is ",Division)
---
+++ Task 2 Create a Personalized Greeting.py
@@ -0,0 +1,3 @@
+x=str(input("Enter your first name :"))
+y=str(input("Enter ypur sencond name :"))
+print("Hello, ",x+" "+y,"! Welcome to the Python Language.")
